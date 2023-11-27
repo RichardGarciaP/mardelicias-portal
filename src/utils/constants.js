feeba215671abf76a5eb2fbe163a60d0984e85d6
RichardGarcia-AS <68579638+RichardGarciaP@users.noreply.github.com>
2023-11-27 00:11:06 -0500

    fix: add user type

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,10 @@
-export const USERS_TYPE = ['admin', 'bodeguero', 'repartidor'];
+export const USERS_TYPE = ['admin', 'bodeguero', 'repartidor', 'cliente'];
 
 export const USERS_TYPE_DEFINITIONS = {
   ADMIN: 'admin',
   WINEMARKER: 'bodeguero',
-  DRIVER: 'repartidor'
+  DRIVER: 'repartidor',
+  CLIENT: 'cliente'
 };
 
 export const ORDER_STATUS = ['procesando', 'enviado', 'entregado'];
